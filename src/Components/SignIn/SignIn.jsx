3c9846de1c724b3a22e14247fd12a5fc21300f23
ayoam <ayoub.amkhazzou.me@gmail.com>
2022-07-05 13:49:44 +0100

    fixed some styles

--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -14,7 +14,7 @@ const SignIn = () => {
   }
   return (
     <div className="z-50 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center ">
-      <h1 className="font-semibold text-3xl mb-6 text-purple-900">Welcome to fireChat</h1>
+      <h1 className="font-semibold text-3xl mb-6 text-purple-900 text-center">Welcome to fireChat</h1>
       <button onClick={signInWithGoogleHandler} className=" bg-slate-100 p-3 w-48 hover:bg-slate-200 flex justify-center items-center shadow-md font-semibold"><FcGoogle size={18}/><p className="ml-2">Sign in with google</p></button>
     </div>
   )
